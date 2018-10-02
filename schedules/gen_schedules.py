e143ce860c5ff365c10209171a0e5d8b7b9ef8a5
Michael Manis <michaelmanis@tutanota.com>
2018-10-02 00:47:06 -0400

    add more schedules

--- schedules/gen_schedules.py
+++ schedules/gen_schedules.py
@@ -1,22 +1,26 @@
 import datetime
 import json
-from itertools import cycle
+from itertools import cycle, repeat
+import os
 
 
 def main():
-    for filename in ['red_weekday.json']:
-        with open(filename) as cfg_file:
+    for filename in ['gray_weekday.json']:
+        with open(os.path.join(os.getcwd(), filename)) as cfg_file:
             cfg = json.load(cfg_file)
         print('-' * len(filename))
         print(filename)
         print('-' * len(filename))
         for cur_cfg in cfg:
-            end_time = datetime.datetime.strptime(cur_cfg[0]['end_time'], '%I:%M%p')
-            column_start_times = [datetime.datetime.strptime(c['start_time'], '%I:%M%p') for c in cur_cfg]
-            column_patterns = [cycle(c['pattern']) for c in cur_cfg]
-            column_firsts = [True for _ in cur_cfg]
+            start_time = datetime.datetime.strptime(cur_cfg['start_time'], '%I:%M%p')
+            end_time = datetime.datetime.strptime(cur_cfg['end_time'], '%I:%M%p')
+            col_count = len(cur_cfg['spacing']) + 1
+            column_start_times = [start_time]
+            column_start_times.extend([start_time + datetime.timedelta(minutes=s) for s in cur_cfg['spacing']])
+            column_patterns = [cycle(cur_cfg['pattern']) for _ in range(col_count)]
+            column_firsts = [True for _ in range(col_count)]
             while column_start_times[0] != end_time:
-                for count, column in enumerate(cur_cfg):
+                for count in range(col_count):
                     pattern = column_patterns[count]
                     if not column_firsts[count]:
                         column_start_times[count] += datetime.timedelta(minutes=pattern.__next__())
