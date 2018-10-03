1bc47bd4e19ae3036a439ad67d7acd7983dff57a
Michael Manis <michaelmanis@tutanota.com>
2018-10-02 21:06:05 -0400

    set up multiprocessing

--- stevens_shuttles.py
+++ stevens_shuttles.py
@@ -1,6 +1,8 @@
-from configparser import ConfigParser
-import multiprocessing
+from multiprocessing import Process
 import os
+from configparser import ConfigParser
+from typing import List
+import time
 
 import psycopg2
 
@@ -19,21 +21,24 @@ def parse_config(file: str ='database.ini', db_type: str ='postgresql'):
     return data
 
 
-def process_shuttle(shuttle: ShuttleService.Shuttle):
-    return f'Processed shuttle {shuttle.id}|{shuttle.timestamp}'
+def process_shuttle(scheduler: ScheduleManager.ScheduleManager, shuttle: ShuttleService.Shuttle):
+    db: psycopg2 = psycopg2.connect(**parse_config())
+    # TODO check whether shuttle has passed a stop and record it in the DB
+    print(f'Processed shuttle {shuttle.id}|{shuttle.timestamp}')
+    db.close()
 
 
 def main():
-    db: psycopg2 = psycopg2.connect(**parse_config())
-
     sm = ShuttleService.ShuttleManager(307)
     with ScheduleManager.SharedScheduleManager() as manager:
         scheduler: ScheduleManager.ScheduleManager = manager.ScheduleManager(307, os.path.join(os.getcwd(), 'schedules', 'generated'))
-        sched = scheduler.paper_schedules()
-    with multiprocessing.Pool(8) as p:
-        res = p.map_async(process_shuttle, sm.shuttles)
-        print(res.get())
-    db.close()
+        workers: List[Process] = []
+        for shuttle in sm.shuttles:
+            p = Process(target=process_shuttle, args=(scheduler, shuttle))
+            p.start()
+            workers.append(p)
+        for worker in workers:
+                worker.join()
     pass
 
 
