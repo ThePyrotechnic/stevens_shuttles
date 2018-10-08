6f29456fa2f4e8d9c5765652c0dc17f36a7c6a19
ThePyrotechnic <michaelmanis@tutanota.com>
2018-10-08 19:56:20 -0400

    add basic tests (#2)
    * add basic tests
    
    * change type checks to isinstance checks

---
+++ lib/test/test_ShuttleService.py
@@ -0,0 +1,19 @@
+import ShuttleService
+
+
+class TestShuttleService:
+    def test_get_routes(self):
+        ss = ShuttleService.ShuttleService(307)
+        assert isinstance(ss.get_routes(), list)
+
+    def test_get_stops(self):
+        ss = ShuttleService.ShuttleService(307)
+        assert isinstance(ss.get_stops(), list)
+
+    def test_get_shuttle_statuses(self):
+        ss = ShuttleService.ShuttleService(307)
+        assert isinstance(ss.get_shuttle_statuses(), list)
+
+    def test_get_stop_ids_for_routes(self):
+        ss = ShuttleService.ShuttleService(307)
+        assert isinstance(ss.get_stop_ids_for_routes(), dict)
---
+++ schedules/test/test_gen_schedules.py
@@ -0,0 +1,8 @@
+import pytest
+
+pytest.skip('Currently no way to test gen_schedules.py', allow_module_level=True)
+
+
+# TODO Clean up gen_schedules.py so that it can be moved into the lib
+class TestGenSchedules:
+    pass
